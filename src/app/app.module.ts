a2ae21e6e63befcbc3392fb553b7d2d83972632a
deondrae21 <deondraespencer0021@gmail.com>
2021-01-12 14:32:48 -0500

    Created reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './home/signup/signup.component';
 import { FormsComponent } from './forms/forms.component';
 import { ContactComponent } from './contact/contact.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -20,7 +25,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forms.component.css']
 })
 export class FormsComponent implements OnInit {
-
+  reportCarForm: FormGroup;
+  reportPersonnelForm: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.reportCarForm = new FormGroup({
+      "userData": new FormGroup({
+        "mechDam": new FormControl(null),
+        "bodyDam": new FormControl(null)
+      }),
+    
+      "moreCarDam": new FormArray([])
+    
+    })
+
+    this.reportPersonnelForm = new FormGroup({
+      "userPersonnelData": new FormGroup({
+        "name": new FormControl(null),
+        "injuries": new FormControl(null)
+      }),
+    
+      "morePersonnelInj": new FormArray([])
+    
+    })
+
+  }
+
+  onAddCar(){
+    const control = new FormControl(null);
+    (<FormArray>this.reportCarForm.get('moreCarDam')).push(control)
+  }
+
+  onAddPerson(){
+    const control = new FormControl(null);
+    (<FormArray>this.reportPersonnelForm.get('morePersonnelInj')).push(control)
   }
 
 }
+
