511a9811eea57440863a12c980ebdb192f8d4ed7
Latiful Ahad <sabitman@hotmail.com>
2021-01-15 12:06:01 -0500

    Removed testFunc from appComp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,19 +12,5 @@ export class AppComponent {
 
   constructor(private userSer: UsersService) {}
 
-  funcToTestService() {
-    let res = this.userSer.LogIn({ username: "sabit", password: "suravi143" });
-  }
-
-  checkState() {
-    this.userSer.LogOut();
-  }
-
-  checkCurrentUsr() { console.log(this.userSer.currentUser) }
-
-  createUser() { 
-    this.userSer.CreateUser("sabit", "suravi143");
-  }
-
-  sendForm() { this.userSer.SendClaim() }
+  
 }
