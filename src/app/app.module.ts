62f23a23003ec9daa41b4c0840fd25f54476bd16
Vinnie Philip <vinnie.philip@gmail.com>
2021-01-19 23:04:28 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from "@angular/forms";
+
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './home/signup/signup.component';
--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/app/auth-gaurd.service';  
+import { FormGroup, FormBuilder, Validators, FormsModule, FormControl } from '@angular/forms';
+import { UsersService } from "src/app/users.service";
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(private authService: AuthGuard, private router: Router, private formBuilder: FormBuilder, private userService: UsersService) {
+
+
+  }
+
+  get usernameControl(){
+    return this.form.get('username') as FormControl;
+  }
+
+  get passwordControl(){
+    return this.form.get('password') as FormControl;
+  }
+
+  get emailControl(){
+    return this.form.get('email') as FormControl;
+  }
+  
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
 }
